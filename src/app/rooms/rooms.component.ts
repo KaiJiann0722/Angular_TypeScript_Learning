37dbd7c846f8fd674366ec69c7a8906223a5b07a
Kai Jiann <kaijianntan0722@gmail.com>
2025-08-17 03:03:19 +0800

    FEAT: ngFor

--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Room } from './rooms';
+import { Room, RoomList } from './rooms';
 
 @Component({
   selector: 'app-rooms',
@@ -20,7 +20,25 @@ export class RoomsComponent implements OnInit {
     bookedRooms: 5
   };
 
-  emptyRoom: Room = {}
+  emptyRoom: Room = {};
+
+  roomList: RoomList[] = [{
+      roomNumber: 1,
+      roomType: 'Deluxe Room',
+      amenities: 'Air conditionar, Free Wifi',
+      price: 500,
+      checkInTime: new Date('11-Aug-2025'),
+      checkOutTime: new Date('12-Aug-2025')  
+    },
+    {
+      roomNumber: 2,
+      roomType: 'Private Suite',
+      amenities: 'Air conditionar, Free Wifi',
+      price: 1000,
+      checkInTime: new Date('11-Aug-2025'),
+      checkOutTime: new Date('12-Aug-2025')  
+    },
+  ]
 
   ngOnInit(): void {
     throw new Error('Method not implemented.');
--- src/app/rooms/rooms.ts
+++ src/app/rooms/rooms.ts
@@ -3,3 +3,12 @@ export interface Room {
     bookedRooms?: number;
     totalRooms?: number;
 }
+
+export interface RoomList {
+    roomNumber: number,
+    roomType: string;
+    amenities: string;
+    price: number;
+    checkInTime: Date;
+    checkOutTime: Date;
+}
