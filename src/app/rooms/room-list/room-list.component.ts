98f1a200b6c9bfed51320f07318d0676f9abd4ad
Kai Jiann <kaijianntan0722@gmail.com>
2025-08-18 01:13:55 +0800

    FEAT: immutable

--- src/app/rooms/room-list/room-list.component.ts
+++ src/app/rooms/room-list/room-list.component.ts
@@ -1,10 +1,11 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { RoomList } from '../rooms';
 
 @Component({
   selector: 'app-room-list',
   templateUrl: './room-list.component.html',
   styleUrl: './room-list.component.css',
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class RoomListComponent implements OnInit {
 
--- src/app/rooms/rooms.component.ts
+++ src/app/rooms/rooms.component.ts
@@ -4,11 +4,10 @@ import { Room, RoomList } from './rooms';
 @Component({
   selector: 'app-rooms',
   templateUrl: './rooms.component.html',
-  styleUrl: './rooms.component.css'
+  styleUrl: './rooms.component.css',
 })
 export class RoomsComponent implements OnInit {
-   
-  hotelName = "Hilton Hotel"
+  hotelName = 'Hilton Hotel';
 
   numberOfRooms = 50;
 
@@ -19,34 +18,34 @@ export class RoomsComponent implements OnInit {
   rooms: Room = {
     totalRooms: 20,
     availableRooms: 10,
-    bookedRooms: 5
+    bookedRooms: 5,
   };
 
   emptyRoom: Room = {};
 
-  roomList: RoomList[] = []
+  roomList: RoomList[] = [];
 
   ngOnInit(): void {
     this.roomList = [
       {
-      roomNumber: 1,
-      roomType: 'Deluxe Room',
-      amenities: 'Air conditionar, Free Wifi',
-      price: 500,
-      checkInTime: new Date('11-Aug-2025'),
-      checkOutTime: new Date('12-Aug-2025'),
-      rating: 4.5
-    },
-    {
-      roomNumber: 2,
-      roomType: 'Private Suite',
-      amenities: 'Air conditionar, Free Wifi',
-      price: 1000,
-      checkInTime: new Date('11-Aug-2025'),
-      checkOutTime: new Date('12-Aug-2025'),
-      rating: 2.6
-    },
-    ]
+        roomNumber: 1,
+        roomType: 'Deluxe Room',
+        amenities: 'Air conditionar, Free Wifi',
+        price: 500,
+        checkInTime: new Date('11-Aug-2025'),
+        checkOutTime: new Date('12-Aug-2025'),
+        rating: 4.5,
+      },
+      {
+        roomNumber: 2,
+        roomType: 'Private Suite',
+        amenities: 'Air conditionar, Free Wifi',
+        price: 1000,
+        checkInTime: new Date('11-Aug-2025'),
+        checkOutTime: new Date('12-Aug-2025'),
+        rating: 2.6,
+      },
+    ];
   }
 
   toggle(): void {
@@ -55,7 +54,21 @@ export class RoomsComponent implements OnInit {
 
   submitRoom(room: RoomList) {
     console.log(room);
-    this.selectedRoom = room
+    this.selectedRoom = room;
   }
 
+  addRoom() {
+    const room: RoomList = {
+      roomNumber: 3,
+      roomType: 'Super Deluxe Room',
+      amenities: 'Good',
+      price: 1500,
+      checkInTime: new Date('18-Aug-2025'),
+      checkOutTime: new Date('18-Aug-2025'),
+      rating: 4.5,
+    };
+    // this.roomList.push(room);
+
+    this.roomList = [...this.roomList, room]
+  }
 }
