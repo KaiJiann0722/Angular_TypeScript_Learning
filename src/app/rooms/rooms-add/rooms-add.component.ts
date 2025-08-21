476321a4ae5c6fd9f1caf2c231e808e4950ddc2b
Kai Jiann <kaijianntan0722@gmail.com>
2025-08-21 22:10:13 +0800

    FEAT: validation input

--- src/app/rooms/rooms-add/rooms-add.component.ts
+++ src/app/rooms/rooms-add/rooms-add.component.ts
@@ -1,31 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 import { RoomList } from '../rooms';
 import { RoomsService } from '../services/rooms.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-rooms-add',
   templateUrl: './rooms-add.component.html',
-  styleUrl: './rooms-add.component.css'
+  styleUrl: './rooms-add.component.css',
 })
 export class RoomsAddComponent implements OnInit {
-
   rooms: RoomList = {
     roomType: '',
     amenities: '',
     price: 0,
     checkInTime: new Date(),
     checkOutTime: new Date(),
-    rating: 0
-  } 
+    rating: 0,
+  };
 
-  successMessage: string = "";
+  successMessage: string = '';
 
-  constructor(private roomService: RoomsService){}
-  
-  ngOnInit(): void {
-  }
+  constructor(private roomService: RoomsService) {}
 
-  addRoom() {
-    this.roomService.addRooms(this.rooms).subscribe(data => this.successMessage = "Rooms add Successfully");
+  ngOnInit(): void {}
+
+  addRoom(roomsForm: NgForm) {
+    this.roomService.addRooms(this.rooms).subscribe((data) => {
+      this.successMessage = 'Rooms add Successfully';
+      roomsForm.reset();
+    });
   }
 }
